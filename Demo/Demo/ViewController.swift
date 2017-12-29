286c6f3b93711747cbefbf1fe63301e0fda5b978
hayasilin <harvey.hayasi@gmail.com>
2017-12-30 06:11:52 +0800

    Add integration demo project

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Kuan-Wei Lin on 12/30/17.
+//  Copyright © 2017 cracktheterm. All rights reserved.
+//
+
+import UIKit
+
+import FrameworkDemo
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let test = TestDemo()
+        test.doTest()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
